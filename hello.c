8688e8bb8b74c65690a95ebb227b0e19b4c64938
Luke Haney <lhaney2@huskers.unl.edu>
2023-08-24 09:53:17 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: NAME Luke Haney
+ *         EMAIL lhaney2@unl.edu
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+
+  printf("I love the School of Computing.\n");
+
+  return 0;
+}
